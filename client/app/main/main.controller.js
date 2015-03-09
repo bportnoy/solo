c42dfc292cc7a40aca85a8805b3a24ead7d9fba6
Bradley Portnoy <bradley.portnoy@gmail.com>
2015-03-09 13:37:31 -0700

    Scaffold project.

---
+++ client/app/main/main.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('soloApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.awesomeThings = [];
+
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+
+  });
