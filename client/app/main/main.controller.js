fd3eaf7205fb220319296677acd68b5eb4dd97d2
Bradley Portnoy <bradley.portnoy@gmail.com>
2015-03-09 17:43:12 -0700

    Fix bug in e-mail submission form.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,6 +2,39 @@
 
 angular.module('inviteMe')
   .controller('MainCtrl', ['$scope', 'RequestInvite', function ($scope, RequestInvite) {
-    RequestInvite('b@b.com');
+    $scope.alerts = [{type:'danger',msg:'hello!'}];
 
+     $scope.addAlert = function(alert) {
+       $scope.alerts.push(alert);
+       console.log(alert);
+       console.log($scope.alerts);
+     };
+
+     $scope.closeAlert = function(index) {
+       $scope.alerts.splice(index, 1);
+     };
+
+    $scope.requestInvite = function(){
+      RequestInvite($scope.emailInput)
+      .success(function(data, status, headers, config){
+        $scope.emailInput = '';
+        $scope.addAlert({
+          type: 'success',
+          msg: 'Thanks! We\'ll let you know when we have space in our beta.'
+          });
+        })
+        .error(function(data, status, headers, config){
+          if (status === 409){
+            $scope.addAlert({
+              type: 'danger',
+              msg: 'It looks like you already requested an invitation with that e-mail address.\n We\'ll be in touch as soon as we have room.'
+            });
+          } else {
+              $scope.addAlert({
+              type: 'danger',
+              msg: 'Something went wrong, and it\'s our fault. Please try again later. You don\'t deserve this.'
+              });
+            }
+        });
+    };
   }]);
